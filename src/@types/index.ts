5850e2faefe43a2bfbd7316c67b3b43753493f54
deafpenguin <victormurilo@gmail.com>
2022-09-13 12:13:53 -0300

    Adicionando novos endpoints e refatorando rotas

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,14 +1,18 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express from 'express';
+import statusRoute from '../routes/status.route';
 import usersRoute from '../routes/users.route';
 
 const app = express();
 
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Configurações de Rotas
 app.use(usersRoute);
+app.use(statusRoute);
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({foo: 'Sucesso total! Atualização em tempo real ocorrendo.'});
-});
-
+// Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação rodando na porta 3000!');
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,21 +1,33 @@
-import { Router, Request, Response, NextFunction } from 'express';
-
-// get /users
-// get /users/:uuid
-// post /users
-// put /users/:uuid
-// delete /users/:uuid
+import { NextFunction, Request, Response, Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
-    const users = [{ userName: 'Victor'}];
-    res.status(200).send(users);
+    const users = [{ userName: 'Victor' }];
+    res.status(StatusCodes.OK).send(users);
 });
 
-usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) =>{
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({uuid});
+    res.status(StatusCodes.OK).send({ uuid });
 });
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser)
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+    modifiedUser.uuid = uuid;
+    res.status(StatusCodes.OK).send(modifiedUser);
+})
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
+})
 
 export default usersRoute;
